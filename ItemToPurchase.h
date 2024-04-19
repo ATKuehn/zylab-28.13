2c07b4f5d607d093d61f0a79ec1c60bef837f321
ATKuehn <167126404+ATKuehn@users.noreply.github.com>
2024-04-19 13:15:46 -0500

    Update ItemToPurchase.h

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,9 +1,22 @@
 #ifndef ITEM_TO_PURCHASE_H
+
 #define ITEM_TO_PURCHASE_H
 
 #include <string>
+
 using namespace std;
-
-/* Type your code here */
-
+class ItemToPurchase {
+public:
+    ItemToPurchase();
+    void SetName(const string& name);
+    string GetName() const;
+    void SetPrice(int price);
+    int GetPrice() const;
+    void SetQuantity(int quantity);
+    int GetQuantity() const;
+private:
+    string itemName;    // Initialized in default constructor to "none"
+    int itemPrice;      // Initialized in default constructor to 0
+    int itemQuantity;   // Initialized in default constructor to 0
+};
 #endif
